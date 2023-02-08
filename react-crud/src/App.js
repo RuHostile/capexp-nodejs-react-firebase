acc752c524232d2647a47a1b9ed947d8332d2794
Dante <db470@student.le.ac.uk>
2023-02-08 16:56:18 +0000

    added and completed project-list project add-project UI components

--- react-crud/src/App.js
+++ react-crud/src/App.js
@@ -3,6 +3,14 @@ import { Link, Route, Routes,  } from "react-router-dom";
 import User from "./components/user.component";
 import UsersList from "./components/users-list.component";
 import AddUser from "./components/add-user.component";
+import Project from "./components/project.component";
+import ProjectList from "./components/projects-list.component";
+import AddProject from "./components/add-project.component";
+// import Expense from "./components/expense.component";
+// import ExpenseList from "./components/expenses-list.component";
+// import AddExpense from "./components/add-expense.component";
+
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
 
@@ -11,7 +19,7 @@ class App extends Component {
     return (
       <div>
         <nav className="navbar navbar-expand navbar-dark bg-dark">
-          <a href="/users" className="navbar-brand">
+          <a href="/" className="navbar-brand">
             CapExp
           </a>
           <div className="navbar-nav mr-auto">
@@ -21,27 +29,27 @@ class App extends Component {
               </Link>
             </li>
             <li className="nav-item">
-              <Link to={"/add"} className="nav-link">
+              <Link to={"/addUser"} className="nav-link">
                 New User
               </Link>
             </li>
             <li className="nav-item">
-              <Link to={""} className="nav-link">
+              <Link to={"/projects"} className="nav-link">
                 Projects
               </Link>
             </li>
             <li className="nav-item">
-              <Link to={""} className="nav-link">
+              <Link to={"/addProject"} className="nav-link">
                 New Project
               </Link>
             </li>
             <li className="nav-item">
-              <Link to={""} className="nav-link">
+              <Link to={"/expenses"} className="nav-link">
                 Expenses
               </Link>
             </li>
             <li className="nav-item">
-              <Link to={""} className="nav-link">
+              <Link to={"/addExpense"} className="nav-link">
                 New Expense
               </Link>
             </li>
@@ -51,10 +59,19 @@ class App extends Component {
 
         <div className="container mt-3">
           <Routes>
-            <Route path="/" element={<UsersList/>} />
+            <Route path="/" element={"Landing Page"} />
             <Route path="/users" element={<UsersList/>} />
-            <Route path="/add" element={<AddUser/>} />
+            <Route path="/addUser" element={<AddUser/>} />
             <Route path="/users/:id" element={<User/>} />
+
+            <Route path="/projects" element={<ProjectList/>} />
+            <Route path="/addProject" element={<AddProject/>} />
+            <Route path="/projects/:id" element={<Project/>} />
+
+            <Route path="/expenses" element={"expenses list"} />
+            <Route path="/addExpense" element={"add expense"} />
+            <Route path="/expenses/:id" element={"expense details"} />
+
           </Routes>
         </div>
       </div>
---
+++ react-crud/src/components/projects-list.component.js
@@ -0,0 +1,191 @@
+import React, { Component } from "react";
+import ProjectDataService from "../services/project.service";
+import { Link } from "react-router-dom";
+
+export default class ProjectsList extends Component {
+  constructor(props) {
+    super(props);
+    this.onChangeSearchProjectName = this.onChangeSearchProjectName.bind(this);
+    this.retrieveProjects = this.retrieveProjects.bind(this);
+    this.refreshList = this.refreshList.bind(this);
+    this.setActiveProject = this.setActiveProject.bind(this);
+    this.removeAllProjects = this.removeAllProjects.bind(this);
+    this.searchProjectName = this.searchProjectName.bind(this);
+
+    this.state = {
+      projects: [],
+      currentProject: null,
+      currentIndex: -1,
+      searchProjectName: "",
+    };
+  }
+
+  componentDidMount() {
+    this.retrieveProjects();
+  }
+
+  onChangeSearchProjectName(e) {
+    const searchProjectName = e.target.value;
+
+    this.setState({
+      searchProjectName: searchProjectName,
+    });
+  }
+
+  retrieveProjects() {
+    ProjectDataService.getAll()
+      .then((response) => {
+        this.setState({
+          projects: response.data,
+        });
+        console.log(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
+  refreshList() {
+    this.retrieveProjects();
+    this.setState({
+      currentProject: null,
+      currentIndex: -1,
+    });
+  }
+
+  setActiveProject(project, index) {
+    this.setState({
+      currentProject: project,
+      currentIndex: index,
+    });
+  }
+
+  removeAllProjects() {
+    ProjectDataService.deleteAll()
+      .then((response) => {
+        console.log(response.data);
+        this.refreshList();
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
+  searchProjectName() {
+    ProjectDataService.findByProjectName(this.state.searchProjectName)
+      .then((response) => {
+        this.setState({
+          projects: response.data,
+        });
+        console.log(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
+  render() {
+    const { searchProjectName, projects, currentProject, currentIndex } = this.state;
+
+    return (
+      <div className="list row">
+        <div className="col-md-8">
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Search by Project Name"
+              value={searchProjectName}
+              onChange={this.onChangeSearchProjectName}
+            />
+            <div className="input-group-append">
+              <button
+                className="btn btn-outline-secondary"
+                type="button"
+                onClick={this.searchProjectName}
+              >
+                Search
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <h4>Projects List</h4>
+
+          <ul className="list-group">
+            {projects &&
+              projects.map((project, index) => (
+                <li
+                  className={
+                    "list-group-item " +
+                    (index === currentIndex ? "active" : "")
+                  }
+                  onClick={() => this.setActiveProject(project, index)}
+                  key={index}
+                >
+                  {project.projectName}
+                </li>
+              ))}
+          </ul>
+
+          <button
+            className="m-3 btn btn-sm btn-danger"
+            onClick={this.removeAllProjects}
+          >
+            Remove All
+          </button>
+        </div>
+        <div className="col-md-6">
+          {currentProject ? (
+            <div>
+              <h4>Project</h4>
+              <div>
+                <label>
+                  <strong>Project Name:</strong>
+                </label>{" "}
+                {currentProject.projectName}
+              </div>
+              <div>
+                <label>
+                  <strong>Description:</strong>
+                </label>{" "}
+                {currentProject.description}
+              </div>
+              <div>
+                <label>
+                  <strong>Department:</strong>
+                </label>{" "}
+                {currentProject.department}
+              </div>
+              <div>
+                <label>
+                  <strong>Captial Expenditure:</strong>
+                </label>{" "}
+                {currentProject.capexp}
+              </div>
+              <div>
+                <label>
+                  <strong>Status:</strong>
+                </label>{" "}
+                {currentProject.published ? "Published" : "Pending"}
+              </div>
+               
+              <Link
+                to={"/projects/" + currentProject.id}
+                type="button"
+                className="btn btn-warning"
+              >
+                Edit
+              </Link>
+            </div>
+          ) : (
+            <div>
+              <br />
+              <p>Please click on a Project...</p>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
